c0b30e6183b277d84046fbd8db8f24a9f7a39f16
Sergi Juanati <wint3rmute@gmail.com>
2020-11-20 18:56:31 +0100

    Added SafeMath & withdrawETH

--- contracts/TokenManager.sol
+++ contracts/TokenManager.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.6.5;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
 
 // https://ethereum.stackexchange.com/questions/10864/erc20-token-and-effective-way-to-store-dividends
 
@@ -14,6 +16,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @title Manages ERC20 & ERC721 tokens
 
 contract TokenManager is Ownable {
+    using SafeMath for uint256;
     address private ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
 
     mapping(string => address) public contractAddressERC20;
@@ -27,15 +30,32 @@ contract TokenManager is Ownable {
     constructor() public {
     }
 
+    /**
+     * @notice  Transfer ether from sender address to contract address
+     * @dev     - Amount to be deposited must be greater than 0 ethers 
+     */
     function depositETH() external payable {
         require(msg.value > 0, "Amount must be greater than 0 ethers");
-        balancesETH[msg.sender] += msg.value;
-        //emit DepositEther(msg.sender, msg.value);  
+        balancesETH[msg.sender] = balancesETH[msg.sender].add(msg.value);
+    }
+
+    /**
+     * @notice  Transfer ether from contract address to sender address
+     *          - Contract must have enough balance to do the transfer
+     * @param   amount Amount of ether to be transferred
+     */
+    function withdrawETH(uint256 amount) public {
+        require(payable(address(this)).balance >= amount, 
+            "Insufficient funds in contract");
+        require(balancesETH[msg.sender] >= amount,
+            "Insufficient balance from user in contract");
+        balancesETH[msg.sender] = balancesETH[msg.sender].sub(amount);
+        address payable recipient = msg.sender;
+        recipient.transfer(amount);
     }
     
     function balanceOf(address _from) public view returns (uint256) {
         return balancesETH[_from];
     }
-
 
 }
