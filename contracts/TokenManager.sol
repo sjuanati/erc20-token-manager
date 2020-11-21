2dc9ea2ba5053c31e861d2168af5f109b46340f8
Sergi Juanati <wint3rmute@gmail.com>
2020-11-21 09:47:26 +0100

    Register & Unregister ERC20 contracts

--- contracts/TokenManager.sol
+++ contracts/TokenManager.sol
@@ -19,15 +19,43 @@ contract TokenManager is Ownable {
     using SafeMath for uint256;
     address private ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
 
-    mapping(string => address) public contractAddressERC20;
-    mapping(string => address) public contractAddressERC721;
+    mapping(string => address) public contractERC20;
+    mapping(string => address) public contractERC721;
 
     mapping(address => uint256) public balancesETH;
     mapping(address => uint256) public balancesERC20;
     mapping(address => uint256) public balancesERC721;
 
+    event DepositEther(address indexed from, uint256 amount);
+    event WithdrawEther(address indexed to, uint256 amount);
+
     // Contract constructor
-    constructor() public {
+    constructor(string memory network) public {
+        // Register DAI & UNI contract addresses
+        if (compareStrings(network, "ganachina")) {
+            registerERC20("DAI", 0xaD6D458402F60fD3Bd25163575031ACDce07538D);
+            registerERC20("UNI", 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
+        }
+    }
+
+    function compareStrings(string memory s1, string memory s2) private pure returns(bool){
+        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    }
+
+    function registerERC20(string memory _tokenName, address _tokenContract) public onlyOwner {
+        require(bytes(_tokenName).length != 0,
+            "Token name can't be empty");
+        require(contractERC20[_tokenName] == ZERO_ADDRESS,
+            "ERC20 contract address already registered");
+        require(bytes(_tokenName).length < 30,
+            "Token name length too long (should be <30 characters)");
+        contractERC20[_tokenName] = _tokenContract;
+    }
+
+    function unregisterERC20(string memory _tokenName) public onlyOwner {
+        require(contractERC20[_tokenName] != ZERO_ADDRESS,
+            "ERC20 contract address already registered");
+        contractERC20[_tokenName] = ZERO_ADDRESS;
     }
 
     /**
@@ -37,6 +65,7 @@ contract TokenManager is Ownable {
     function depositETH() external payable {
         require(msg.value > 0, "Amount must be greater than 0 ethers");
         balancesETH[msg.sender] = balancesETH[msg.sender].add(msg.value);
+        emit DepositEther(msg.sender, msg.value);
     }
 
     /**
@@ -52,6 +81,7 @@ contract TokenManager is Ownable {
         balancesETH[msg.sender] = balancesETH[msg.sender].sub(amount);
         address payable recipient = msg.sender;
         recipient.transfer(amount);
+        emit WithdrawEther(msg.sender, amount);
     }
     
     function balanceOf(address _from) public view returns (uint256) {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,9 @@
 const TokenManager = artifacts.require("TokenManager");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenManager);
+module.exports = (deployer, network, accounts) => {
+	// if (accounts) {
+	// 	// Create contract with 1 ether (contract must be payable)
+	// 	deployer.deploy(TokenManager, network, { from: accounts[0], value: "1000000000000000000" });
+	// };
+	deployer.deploy(TokenManager, network);
 };
-
